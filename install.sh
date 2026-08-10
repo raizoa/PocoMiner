b4397b90275ff11742387ac6f9f771d9c3081256
raizoa <raizoakbar@gmail.com>
2026-08-10 23:29:04 +0700

    fix installer v1.1

--- install.sh
+++ install.sh
@@ -4,28 +4,20 @@ echo "===== PocoMiner LTC Installer ====="
 
 DIR=$HOME/PocoMiner-LTC
 
-mkdir -p $DIR
-
-cd $DIR
-
+mkdir -p "$DIR"
+cd "$DIR"
 
 echo "[1] Installing dependency"
-
 pkg update -y
 pkg install -y wget curl
 
-
 echo "[2] Download XMRig"
-
 wget -O xmrig \
 https://raw.githubusercontent.com/raizoa/PocoMiner/main/xmrig
 
-
 echo "[3] Download config"
-
 wget -O config_ltc.json \
 https://raw.githubusercontent.com/raizoa/PocoMiner/main/config_ltc.json
-
 
 echo "[4] Download scripts"
 
@@ -35,26 +27,27 @@ wget -O $f \
 https://raw.githubusercontent.com/raizoa/PocoMiner/main/$f
 done
 
-
 echo "[5] Permission"
 
 chmod +x xmrig
 chmod +x *.sh
 
+touch miner_ltc.log
 
 echo
 echo "Testing XMRig"
 
 ./xmrig --version
 
-
 echo
-echo "=============================="
-echo " PocoMiner LTC Ready"
-echo "=============================="
+echo "Installation Complete"
+echo
+
+ls -lh
 
 echo
 echo "Start:"
+echo "cd ~/PocoMiner-LTC"
 echo "./start_ltc.sh"
 
 echo
